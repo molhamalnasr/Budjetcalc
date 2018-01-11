5866d5e6ed41500b825a228939d56d8e538a4639
Molham <dr.molham93@gmail.com>
2018-01-11 16:29:28 +0100

    clea up the test code

--- app.js
+++ app.js
@@ -1,17 +1,7 @@
 //Creat the module pattern
 var budgetController = (function(){
-    //Test
-    var x = 26;
     
-    var add = function(a){
-        return x + a;
-    }
     
-    return {
-        publichTest: function(b){
-            console.log(add(b));
-        }
-    }
     
 })();
 
